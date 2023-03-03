ef6c268f6d899e3b09386c88adddb612b729173c
Gabriel <gabriel.d.machado92@gmail.com>
2023-03-03 17:32:06 -0300

    created models, introduced authtoken with managers endpoint

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('test/', views.test),
+    path('groups/manager/users', views.managers),
 ]
--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAdminUser
+
+from django.contrib.auth.models import User, Group
 
 # Create your views here.
 @api_view()
 def test(request):
     return Response('This is a test', status.HTTP_200_OK)
+
+@api_view(['POST', 'DELETE', 'GET'])
+@permission_classes([IsAdminUser])
+def managers(request):
+    username = request.data['username']
+    if username:
+        user = get_object_or_404(User, username=username)
+        managers = Group.objects.get(name='Manager')
+        if request.method == 'POST':
+            managers.user_set.add(user)
+            return Response({'message:':'User set to manager group'}, status=status.HTTP_200_OK)
+        elif request.method == 'DELETE':
+            managers.user_set.remove(user)
+            return Response({'message:':'User deleted from manager group'}, status=status.HTTP_200_OK)
+    return Response({'message:':'There was a problem in this endpoint'}, status=status.HTTP_400_BAD_REQUEST)
